7b112112af8c099ea6dc8376f8e8c0d0834dd36a
Robin <Robinmazouni@outlook.com>
2022-04-30 11:21:26 -0400

    Ajout Route Config

--- React/bibliotheque/src/index.js
+++ React/bibliotheque/src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path='/' element={<App />}/>
+        <Route path='/home' element={<HomeClient />}/>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
