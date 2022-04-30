1776120c42b72c441a1bb172d4ad307969677324
Robin <Robinmazouni@outlook.com>
2022-04-30 11:17:21 -0400

    Ajout Header Dynamic for Client, Employe, Admin

---
+++ React/bibliotheque/src/components/Header/Header.jsx
@@ -0,0 +1,75 @@
+import { Link } from "react-router-dom";
+
+
+const headerClient = {
+    home: 'Home',
+    searchDocument: 'Reherche de document',
+    borrowDocs: 'Emprunts',
+    bills: 'Factures'
+}
+
+const headerEmploye = {
+    home: 'Home',
+    clients: 'Clients',
+    searchDocument: 'Reherche de document',
+    borrowDocs: 'Emprunts',
+    bills: 'Factures'
+}
+
+const headerAdmin = {
+    home: 'Home',
+    client: 'Clients',
+    employes: 'Employes',
+    searchDocument: 'Reherche de document',
+    borrowDocs: 'Emprunts',
+    bills: 'Factures'
+}
+
+export default function Header({ headerFor }){
+
+
+
+
+
+    const header = () => {
+        if(headerFor === 'client' ){
+            return headerForUser(headerClient)
+        } else if(headerFor === 'employe' ){
+            return headerForUser(headerEmploye);
+        } else return headerForUser(headerAdmin);
+    }
+
+
+    const headerForUser = (header) => Object.keys(header).map(
+        (field) => {
+            return (
+                <>
+                    <Link to={'/'+field}>{header[field]}</Link> | { " " }
+                </>
+            );
+        }
+    )
+
+    return (
+        <div>
+        <h1>CLient</h1>
+        <nav
+          style={{
+            borderBottom: "solid 1px",
+            paddingBottom: "1rem",
+          }}
+        >
+        
+        {header()}
+        </nav>
+      </div>
+    )
+}
+
+/* 
+        <Link to="/home">home</Link> |{" "}
+          <Link to="/searchDocument">Reherche de document</Link> |{" "}
+          <Link to="/borrowDocs">Emprunts</Link> |{" "}
+          <Link to="/bills">Factures</Link> |{" "}
+
+*/
